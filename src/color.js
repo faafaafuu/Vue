af54835cb2cbeeb4e7711f6d4ed615257ba7604f
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:03:59 +0300

    Модификаторы

--- src/color.js
+++ src/color.js
@@ -1,8 +1,22 @@
 export default {
   bind(el, bindings, vnode) {
-    // el.style.color = 'blue'
-    const arg = bindings.arg
+    const fontModifier = bindings.modifiers['font']
 
-    el.style[arg] = bindings.value
+    if (fontModifier) {
+      el.style.fontSize = '30px'
+    }
+
+    const delayModifier = bindings.modifiers['delay']
+
+    let delay = 0
+
+    if (delayModifier) {
+      delay = 2000
+    }
+
+    setTimeout(() => {
+      const arg = bindings.arg
+      el.style[arg] = bindings.value
+    }, delay)
   }
 }
