016da490d4a8ed88159507cb234ce4d40dfbbadd
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 03:59:25 +0300

    Жизненный цикл директивы

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h2 v-colored>{{ title }}</h2>
+    <h2 v-colored v-if="visible">{{ title }}</h2>
+
+    <button @click="visible = !visible">Toggle</button>
+    <button @click="title = 'New title'">Change title</button>
   </div>
 </template>
 
@@ -9,7 +12,8 @@
 export default {
   data () {
     return {
-      title: 'Hello I am Vue!'
+      title: 'Hello I am Vue!',
+      visible: true
     }
   }
 }
