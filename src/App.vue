42f6bfd70faa6601b6912a8433fb9c8206141ae2
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:12:23 +0300

    Текстовый инпут

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
 <template>
   <div>
-    <h2>{{ title }}</h2>
+    <h2>Form inputs</h2>
+    <input type="text" v-model.lazy="name">
 
-    <input type="text" v-model="searchName">
-
-    <ul>
-      <li v-for="name of filteredNames">{{name}}</li>
-    </ul>
-
-    <hr>
-
-    <app-list></app-list>
+    <p>{{name}}</p>
   </div>
 </template>
 
 <script>
 
-import ListMixin from './listMixin'
-
 export default {
   data () {
     return {
-      title: 'Hello I am Vue!'
-    }
-  },
-  mixins: [ListMixin],
-  filters: {
-    lowercase(value) {
-      return value.toLowerCase()
+      name: 'Initial state'
     }
   }
 }
