c8ee5a6372fa0bce0047539d39535f254dd019b4
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:28:17 +0300

    Создание навигации

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="container">
-    <router-view></router-view>
-  </div>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="'/cars'">Cars</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {}
 </script>
