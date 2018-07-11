2ece497ea5d316097c13dd6e5fcd3e23745e28f7
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:41:22 +0300

    Настройка приложения

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav">
-            <router-link tag="li" class="nav-item" exact to="/" active-class="active">
-              <a class="nav-link">Home</a>
-            </router-link>
-            <router-link tag="li" class="nav-item" to="/cars" active-class="active">
-              <a class="nav-link">Cars</a>
-            </router-link>
+  <div class="container">
 
-            <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
-              <a class="nav-link">Car 3</a>
-            </router-link>
-            <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
-              <a class="nav-link">Car 4</a>
-            </router-link>
-          </ul>
-        </div>
-      </nav>
-
-      <router-view></router-view>
-    </div>
+  </div>
 </template>
 
 <script>
