4adee330f7c951bd5b4a2c6cf44b453c1d1291a7
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:32:06 +0300

    Динамические роуты

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@
             <router-link tag="li" class="nav-item" to="/cars" active-class="active">
               <a class="nav-link">Cars</a>
             </router-link>
+
+            <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
+              <a class="nav-link">Car 3</a>
+            </router-link>
+            <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
+              <a class="nav-link">Car 4</a>
+            </router-link>
           </ul>
         </div>
       </nav>
--- src/pages/Cars.vue
+++ src/pages/Cars.vue
@@ -1,11 +1,24 @@
 <template>
-  <h1>Cars page</h1>
+  <div>
+    <h1>Cars page</h1>
+
+    <ul>
+
+      <router-link
+        tag="li"
+        v-for="car in 5"
+        :to="'/car/' + car"
+        :key="car"
+      >
+        <a>Car {{ car }}</a>
+      </router-link>
+
+    </ul>
+  </div>
 </template>
 
 <script>
-  export default {
-
-  }
+  export default {}
 </script>
 
 <style scoped>
