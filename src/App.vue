3369f028ea87f6ced84565d62a34b28034528b25
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:08:29 +0300

    Фильтрация списков

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
-    <h2>{{ title | lowercase }}</h2>
-    <h2>{{ title | uppercase }}</h2>
-    <h2>{{ title | uppercase | lowercase }}</h2>
+
+    <input type="text" v-model="searchName">
+
+    <ul>
+      <li v-for="name of filteredNames">{{name}}</li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +15,16 @@
 export default {
   data () {
     return {
-      title: 'Hello I am Vue!'
+      title: 'Hello I am Vue!',
+      searchName: '',
+      names: ['Vlad', 'Max', 'Elena', 'Igor']
+    }
+  },
+  computed: {
+    filteredNames() {
+      return this.names.filter(name => {
+        return name.toLowerCase().indexOf(this.searchName.toLowerCase()) !== -1
+      })
     }
   },
   filters: {
