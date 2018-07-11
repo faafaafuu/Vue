0f1009f33000e9fff7dcb83e0dfd47026b38be6e
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:15:32 +0300

    Селекты

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,11 @@
   <div>
     <h2>Form inputs</h2>
 
-    <label>
-      <input type="radio" value="instagram" v-model="social"> Instagram
-    </label>
-
-    <label>
-      <input type="radio" value="vk" v-model="social"> Vk
-    </label>
-
-    <label>
-      <input type="radio" value="facebook" v-model="social"> Facebook
-    </label>
+    <select v-model="social">
+      <option
+        v-for="s in socialsList"
+      >{{ s }}</option>
+    </select>
 
     <hr>
 
@@ -25,7 +19,9 @@
 export default {
   data () {
     return {
-      social: 'facebook'
+      defaultSocial: 'vk',
+      social: 'instagram',
+      socialsList: ['instagram', 'vk', 'facebook']
     }
   }
 }
