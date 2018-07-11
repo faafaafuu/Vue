a69b68a541b14ee86257f4252aa5946495d606b6
farzullaev@bk.ru <farzullaev@bk.ru>
2018-07-11 04:43:13 +0300

    Глобальная настройка

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,8 @@
       return {
         carName: '',
         carYear: 2018,
-        cars: []
+        cars: [],
+        resource: null
       }
     },
     methods: {
@@ -43,23 +44,15 @@
           year: this.carYear
         }
 
-        this.$http.post('http://localhost:3000/cars', car)
-          .then(response => {
-            return response.json()
-          })
-          .then(newCar => {
-            console.log(newCar)
-          })
+        this.resource.save({}, car)
       },
       loadCars () {
-        this.$http.get('http://localhost:3000/cars')
-          .then(response => {
-            return response.json()
-          })
-          .then(cars => {
-            this.cars = cars
-          })
+        this.resource.get().then(response => response.json())
+          .then(cars => this.cars = cars)
       }
+    },
+    created () {
+      this.resource = this.$resource('cars')
     }
   }
 </script>
